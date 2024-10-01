9ca427e3cf88df31fb2e10cf60829269da0b14b9
tistanaiyaa <tistamaharti@gmail.com>
2024-10-01 20:43:49 +0700

    Add files via upload

--- algo2(lat2).py
+++ algo2(lat2).py
@@ -12,7 +12,7 @@ bujur2 = math.radians (float(input("bujur kota 2: ")))
 R = 6371.0
 lat = lintang2 - lintang1
 long = bujur2 - bujur1
-a = math.sin(lat/2)*2 + math.cos(lintang1)*math.cos(lintang2)*math.sin(long/2)*2
+a = math.sin(lat/2)**2 + math.cos(lintang1)*math.cos(lintang2)*math.sin(long/2)**2
 C = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 d = R * C
 print("jarak antara dua titik tersebut adalah ", d, "kilometer")
