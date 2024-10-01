5e545042a0277b66cd8859ecba61c81c17ae3b97
tistanaiyaa <tistamaharti@gmail.com>
2024-10-01 20:17:30 +0700

    Add files via upload

---
+++ algo2(lat2).py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 23:00:17 2024
+
+@author: Lenovo
+"""
+import math
+lintang1 = math.radians (float(input("lintang kota 1: ")))
+bujur1 = math.radians (float(input("bujur kota 1: ")))
+lintang2 = math.radians (float(input("lintang kota 2: ")))
+bujur2 = math.radians (float(input("bujur kota 2: ")))
+R = 6371.0
+lat = lintang2 - lintang1
+long = bujur2 - bujur1
+a = math.sin(lat/2)*2 + math.cos(lintang1)*math.cos(lintang2)*math.sin(long/2)*2
+C = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+d = R * C
+print("jarak antara dua titik tersebut adalah ", d, "kilometer")
---
+++ prak 2 algo.py
@@ -0,0 +1,24 @@
+
+import math
+bilA = float(input("masukkan nilai A: "))
+bilB = float(input("masukkan nilai B: "))
+jumlah = bilA + bilB 
+print(f"hasil dari penjumlahan A + B adalah {jumlah}")
+selisih = bilA - bilB 
+print(f"selisih antara A dan B adalah {selisih}")
+kali = bilA * bilB
+print(f"jumlah A dikali B adalah {kali} ")
+sisa = bilA % bilB
+print(f"jumlah sisa pembagian dari hasil A dibagi B adalah {sisa}")
+bagi = bilA / bilB
+print(f"jumlah A dibagi B adalah {bagi}")
+logA = math.log(bilA)
+print(f"hasil dari logA adalah {logA}")
+pangkat = bilA ** bilB
+print(f"hasil A pangkat B adalah {pangkat}")
+
+
+
+
+
+
